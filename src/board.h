201c6751810236b383b1caae77de8bc0873b9e57
Nathan Fallet <contact@nathanfallet.me>
2022-12-10 15:25:31 +0100

    Better doc for board (test)

--- src/board.h
+++ src/board.h
@@ -1,27 +1,47 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+/**
+ * \file board.h
+ * \brief Gestion d'un plateau
+ * 
+ * Gestion d'un plateau de jeu via l'objet Board.
+ */
+
 #include "constants.h"
 #include "cell.h"
 #include "state.h"
 
+/**
+ * \brief Plateau de jeu
+ * 
+ * Un plateau de jeu est un tableau de 2 entiers non signés.
+ * 
+ * Le premier entier représente les pions noirs.
+ * Le second entier représente les pions blans.
+ * 
+ * Chaque bit représente une cellule du plateau.
+ * 
+ * 0 = Cellule vide
+ * 1 = Cellule pleine
+ */
 typedef unsigned long Board[2];
 
 /**
- * Crée un plateau par défaut
+ * \brief Crée un plateau par défaut
  * \param board Le plateau à créer
  */
 void board_create(Board board);
 
 /**
- * Clone un plateau
+ * \brief Clone un plateau
  * \param board Le plateau à cloner
  * \param target Le plateau cible
  */
 void board_clone(Board board, Board target);
 
 /**
- * Retourne la cellule à la position donnée
+ * \brief Retourne la cellule à la position donnée
  * \param board Le plateau
  * \param line La ligne
  * \param column La colonne
@@ -30,7 +50,7 @@ void board_clone(Board board, Board target);
 Cell board_get_cell(Board board, int line, int column);
 
 /**
- * Modifie la cellule à la position donnée
+ * \brief Modifie la cellule à la position donnée
  * \param board Le plateau
  * \param line La ligne
  * \param column La colonne
@@ -39,7 +59,7 @@ Cell board_get_cell(Board board, int line, int column);
 void board_set_cell(Board board, int line, int column, Cell cell);
 
 /**
- * Retourne l'état du plateau
+ * \brief Retourne l'état du plateau
  * \param board Le plateau
  * \return L'état du plateau
  */
